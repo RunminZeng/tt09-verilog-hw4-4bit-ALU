1cbc2b4d67955d210db63ed8c0f8301ca19bcc6b
RunminZeng <165593003+RunminZeng@users.noreply.github.com>
2024-10-18 23:10:38 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,11 +1,6 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
 `default_nettype none
 
-module tt_um_example (
+module tt_um_koggestone_adder4 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +11,38 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  wire [3:0] A, B;
+  wire [3:0] Sum;
+  wire Cout;
+  
+  assign A = ui_in[3:0];
+  assign B = ui_in[7:4];
+   
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire [3:0] P, G;
+  wire [4:0] C;
+
+
+  assign P = A ^ B;
+  assign G = A & B;
+
+assign C[0] = 0;
+assign C[1] = G[0] | (P[0] & C[0]); 
+assign C[2] = G[1] | (P[1] & C[1]); 
+assign C[3] = G[2] | (P[2] & C[2]); 
+assign C[4] = G[3] | (P[3] & C[3]); 
+
+assign Sum[0] = P[0] ^ C[0]; 
+assign Sum[1] = P[1] ^ C[1]; 
+assign Sum[2] = P[2] ^ C[2]; 
+assign Sum[3] = P[3] ^ C[3]; 
+
+assign Cout = C[4];
+
+assign uo_out[3:0] = Sum;
+assign uo_out[4] = Cout; 
+assign uo_out[7:5] = 3'b000;
+assign uio_out = 8'b00000000;
+assign uio_oe = 8'b00000000;
 
 endmodule
